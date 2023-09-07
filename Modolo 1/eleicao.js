809bf8efb246e7a7db25abf72f72a588df6428a1
igoreramos <139307520+igoreramos@users.noreply.github.com>
2023-09-07 01:57:15 +0000

    06/09/23

--- Modolo 1/eleicao.js
+++ Modolo 1/eleicao.js
@@ -1,4 +1,5 @@
 const readline = require("readline-sync")
+var loop = true;
 function simularEleicao() {
     let votosCandidatoX = 0;
     let votosCandidatoY = 0;
@@ -6,7 +7,7 @@ function simularEleicao() {
     let votosBranco = 0;
     let votosNulo = 0;
 
-    while (true) {
+    while (loop) {
 
       console.log();                
       let voto = parseInt(readline.question(`Digite o número do seu voto \n
@@ -50,7 +51,8 @@ function simularEleicao() {
           break;
           default:
           votosNulo++;
-          alert("Voto nulo: número de candidato inválido.");
+          console.log("Voto nulo: número de candidato inválido.");
+          loop = false;
           break;
         }
          
