cc76e9cbf620294b8c61d3ae79d108fb9db151f5
igoreramos <139307520+igoreramos@users.noreply.github.com>
2023-09-07 02:51:08 +0000

    softex

---
+++ listamodulo1.js
@@ -0,0 +1,14 @@
+const pessoas = ["Fulano1", "Fulano2", "Fulano3", "Fulano4", "Fulano5", "Fulano6", "Fulano7", "Fulano8", "Fulano9", "Fulano10"];
+const idadesDasPessoas = [22, 32, 44, 52, 22, 56, 78, 98, 101, 76];
+const coresDasPessoas = ["Vermelho", "Roxo", "Preto", "Azul", "Amarelo", "Verde", "Branco", "Vermelho", "Azul", "Azul", "Verde"];
+// console.log(pessoas);
+// console.log(idadesDasPessoas);
+// console.log(coresDasPessoas);
+const pessoasGeral = [pessoas, idadesDasPessoas, coresDasPessoas];
+console.log(pessoasGeral);
+
+idadesDasPessoas[0] = "22"
+coresDasPessoas[0] = "roxo"
+console.log(pessoasGeral);
+
+
