c9b286f1a7d5fdd73ec62712bc13111547b15e9e
igoreramos <139307520+igoreramos@users.noreply.github.com>
2023-09-07 20:18:55 +0000

    softex

---
+++ exercicio1.js
@@ -0,0 +1,29 @@
+var boolean;
+console.log(boolean);
+var string;
+console.log(string);
+var symbol;
+console.log(symbol);
+var bigInt;
+console.log(bigInt);
+
+boolean = null;
+console.log(boolean);
+string = null;
+console.log(string);
+symbol = null;
+console.log(symbol);
+bigInt = null;
+console.log(bigInt);
+
+boolean = true;
+console.log(typeof(boolean), boolean);
+string = "carro";
+console.log(typeof(string), string);
+symbol = Symbol("Full")
+console.log(typeof(symbol), symbol);
+bigInt = 123456789101112131415161718192021222324252627282930n
+console.log(typeof(bigInt), bigInt);
+
+
+
