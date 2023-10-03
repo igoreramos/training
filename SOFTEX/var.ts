e39b55d153adbff9b3356f3fb6d14920195828a8
igoreramos <139307520+igoreramos@users.noreply.github.com>
2023-10-03 11:40:08 +0000

    softex

--- SOFTEX/var.ts
+++ SOFTEX/var.ts
@@ -20,35 +20,49 @@
 // var str: string = "Carro"
 // console.log("aqui".lastIndexOf(str));
 
-let veiculos: Array<string> = ["Caminhão", "Moto", "Carro", "Onibus"];
-console.log(veiculos);
+// let veiculos: Array<string> = ["Caminhão", "Moto", "Carro", "Onibus"];
+// console.log(veiculos);
 
-let veiculos2: Array<string> = ["Caminhão2", "Moto2", "Carro2", "Onibus2"]; 
-console.log("Metodo Concat", veiculos2.concat(veiculos));
+// let veiculos2: Array<string> = ["Caminhão2", "Moto2", "Carro2", "Onibus2"]; 
+// console.log("Metodo Concat", veiculos2.concat(veiculos));
 
-veiculos.push("Metro")
-console.log("Metodo push", veiculos);
+// veiculos.push("Metro")
+// console.log("Metodo push", veiculos);
 
-veiculos.pop();
-console.log("Metodo pop", veiculos);
+// veiculos.pop();
+// console.log("Metodo pop", veiculos);
 
-veiculos.shift();
-console.log("Metodo shift", veiculos);
+// veiculos.shift();
+// console.log("Metodo shift", veiculos);
 
-veiculos.unshift("Velocipede")
-console.log("Metodo unshift", veiculos);
+// veiculos.unshift("Velocipede")
+// console.log("Metodo unshift", veiculos);
 
-let veiculos3: string = veiculos.join("* ");
-console.log("Metodo join --> ", veiculos3);
+// let veiculos3: string = veiculos.join("* ");
+// console.log("Metodo join --> ", veiculos3);
 
-veiculos.slice();
-console.log(veiculos);
+// veiculos.slice();
+// console.log(veiculos);
 
-veiculos.push("Bitrem");
-console.log("Metodo push", veiculos);
+// veiculos.push("Bitrem");
+// console.log("Metodo push", veiculos);
 
-veiculos.splice(1, 3)
-console.log("Metodo Splice", veiculos);
+// veiculos.splice(1, 3)
+// console.log("Metodo Splice", veiculos);
+
+enum TipoDeUsuario {
+    Adm,
+    Comum,
+    Convidado,
+};
+function verificarPermissao(usuario: TipoDeUsuario): void {
+    if (usuario === TipoDeUsuario.Adm) {
+        // Realizar ações administrativas
+      } else {
+        // Acesso restrito para outros tipos de usuário
+      }    
+};
+
 
 
 
