89ae9d514bb57092d5128a567c109c78880082a1
igoreramos <139307520+igoreramos@users.noreply.github.com>
2023-10-05 11:49:50 +0000

    softex

--- Atividades_Joyclass_jsM01/calc_media_jsM01.js
+++ Atividades_Joyclass_jsM01/calc_media_jsM01.js
@@ -1,10 +1,10 @@
 const readline = require("readline-sync")
 
 function calcularMedia1() {
-    const nota1 = parseInt(readline.question("Digite a nota de 0 a 10: "))
-    const nota2 = parseInt(readline.question("Digite a nota de 0 a 10: "))
-    const nota3 = parseInt(readline.question("Digite a nota de 0 a 10: "))
+    const nota1 = parseFloat(readline.question("Digite a nota de 0 a 10: "))
+    const nota2 = parseFloat(readline.question("Digite a nota de 0 a 10: "))
+    const nota3 = parseFloat(readline.question("Digite a nota de 0 a 10: "))
     const media = (nota1 + nota2 + nota3) / 3
-    console.log(`A média é: ${media}`);
+    console.log(`A média é: ${media.toFixed(1)}`);
 }
 calcularMedia1()
