a8f70c11f196f8d496aadd0d555557e3d33ed462
Igor Eduardo Ramos <139307520+igoreramos@users.noreply.github.com>
2024-01-02 19:39:25 +0000

    02-01-24

--- Atividades_WebService2/app.js
+++ Atividades_WebService2/app.js
@@ -1,30 +1,55 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-const { appendFileSync } = require('fs');
 
 const porta = 3000;
 const app = express();
 
-const livros = [
-  { id: 1, titulo: "The Lord of the Ring", autor: "J.R.R. Tolkien" },
-  { id: 2, titulo: "The Hobbit: Unexpected Journey", autor: "J.R.R. Tolkien" }
+const usuarios = [
+  { id: 1, nome: "Dexter Jackson" },
+  { id: 2, nome: "Jeremy Reynolds" }
 ];
 
 app.use(bodyParser.json());
 
-app.get('/livros', (req, res) => {
-  res.send(livros);
-});
-
-app.get("/livros/:id", (req, res) => {
-  const livro = livros.find((l) => l.id === parseInt(req.params.id));
-
-  if (!livro) {
-    return res.status(404).json({ mensagem: "Livro não encontrado" });
+app.get('/usuarios', (req, res) => {
+  try {
+    res.send(usuarios)
+  } catch (error) {
+    res.status(404).json({ mensagem: 'Usuários não encontrados' })
   }
-  res.send(livro);
 });
 
+app.get("/usuarios/:id", (req, res) => {
+  const usuario = usuarios.find((l) => l.id === parseInt(req.params.id));
+
+  if (!usuario) {
+    return res.status(404).json({ mensagem: "Usuario não encontrado" });
+  }
+  res.send(usuario);
+});
+
+app.post("/usuarios", (req, res) => {
+  const novoUsuario = {
+    id: usuarios.length + 1,
+    nome: req.body.nome,
+  };
+
+  usuarios.push(novoUsuario);
+  res.status(201).json(novoUsuario);
+});
+
+app.put("/usuarios/:id", (req, res) => {
+  const usuarioIndex = usuarios.findIndex((l) => l.id === parseInt(req.params.id));
+
+  if (usuarioIndex === -1) {
+    return res.status(404).json({ mensagem: "Usuario não encontrado" });
+  }
+  usuarios[usuarioIndex] = {
+    id: usuarios[usuarioIndex].id,
+    nome: req.body.nome,
+  };
+  res.json(usuarios[usuarioIndex]);
+});
 
 
 app.listen(porta, () => {
