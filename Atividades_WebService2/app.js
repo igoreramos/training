c72c92ca8e4f024f9cf8b7432c939ea6deb51f27
Igor Eduardo Ramos <139307520+igoreramos@users.noreply.github.com>
2024-01-02 18:54:50 +0000

    02-01-24

--- Atividades_WebService2/app.js
+++ Atividades_WebService2/app.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Bem-vindo ao meu servidor!');
-});
+const { appendFileSync } = require('fs');
 
 const porta = 3000;
+const app = express();
+
+const livros = [
+  { id: 1, titulo: "The Lord of the Ring", autor: "J.R.R. Tolkien" },
+  { id: 2, titulo: "The Hobbit: Unexpected Journey", autor: "J.R.R. Tolkien" }
+];
+
+app.use(bodyParser.json());
+
+app.get('/livros', (req, res) => {
+  res.send(livros);
+});
+
+app.get("/livros/:id", (req, res) => {
+  const livro = livros.find((l) => l.id === parseInt(req.params.id));
+
+  if (!livro) {
+    return res.status(404).json({ mensagem: "Livro não encontrado" });
+  }
+  res.send(livro);
+});
+
+
 
 app.listen(porta, () => {
   console.log(`Servidor rodando em http://localhost:${porta}`);
